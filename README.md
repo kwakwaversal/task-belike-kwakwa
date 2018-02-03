3c279c63112c1f8b37bad2384aaf2a4ba37d9057
Paul Williams <kwakwaversal@gmail.com>
2018-02-03 20:47:06 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Task::BeLike::KWAKWA - Blah blah blah
+
+# SYNOPSIS
+
+    use Task::BeLike::KWAKWA;
+
+# DESCRIPTION
+
+Task::BeLike::KWAKWA is
+
+# AUTHOR
+
+Paul Williams <kwakwa@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2018- Paul Williams
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Task/BeLike/KWAKWA.pm
@@ -0,0 +1,39 @@
+package Task::BeLike::KWAKWA;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Task::BeLike::KWAKWA - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Task::BeLike::KWAKWA;
+
+=head1 DESCRIPTION
+
+Task::BeLike::KWAKWA is
+
+=head1 AUTHOR
+
+Paul Williams E<lt>kwakwa@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2018- Paul Williams
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
